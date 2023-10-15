4e211fa68f3fe57605292babc860e9522b8739b2
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-10-16 00:40:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // Remove any non-letter characters and convert to lowercase
+  str = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  
+  // Reverse the string
+  const reversedStr = str.split("").reverse().join("");
+  
+  // Check if the reversed string is the same as the original string
+  return str === reversedStr;
 }
 
-/* 
-  Add your pseudocode here
-*/
+// Test cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("robot")); // false
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
+console.log(isPalindrome("Was it a car or a cat I saw?")); // true
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
@@ -20,6 +26,6 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
+
 
 module.exports = isPalindrome;
